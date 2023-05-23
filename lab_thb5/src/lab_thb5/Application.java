eb85309a9eb93392b7ca68031f06e000847c1344
Hiaann0810 <125818312+Hiaann0810@users.noreply.github.com>
2023-05-23 10:57:29 +0700

    Add files via upload

---
+++ lab_thb5/src/lab_thb5/Application.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab_thb5;
+
+/**
+ *
+ * @author vietthanh
+ */
+public class Application {
+    public static void main(String[] agrs) {
+        new Manager().Start();
+
+    }
+    
+}
